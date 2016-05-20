3e718a727a0c400a53e94912c6f38be1b86ed29f
Max Shekhovtcov <mashekho@microsoft.com>
2016-05-20 09:59:48 -0700

    adf

--- react1/src/js/client.js
+++ react1/src/js/client.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
